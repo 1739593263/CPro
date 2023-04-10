a03348bc3ec80968735e8d6a364e928ad6601b22
17395 <invulnerableandre@gmail.com>
2023-04-10 13:54:50 -0500

    C project: 1.2 delete,insert,find,sort functions

--- linkedlist.c
+++ linkedlist.c
@@ -33,12 +33,91 @@ struct node * Create(){
     return head;
 };
 
+void InsertFirst(int value){
+    struct node *newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->val=value;
+    newNode->next=head;
+    head=newNode;
+}
+
+struct node* deleteFirst(){
+    struct node *deletedNode = (struct node*)malloc(sizeof(struct node));
+    deletedNode=head;
+    head=head->next;
+    return head;
+};
+
+struct node* find(int key){
+    struct node* current = (struct node*)malloc(sizeof(struct node));
+    current=head;
+    for(int i=0; i<key; i++){
+        current=current->next;
+    }
+    return current;
+};
+
+struct node* delete(int key){
+    struct node* current = (struct node*)malloc(sizeof(struct node));
+    struct node* previous = (struct node*)malloc(sizeof(struct node));
+    current=head;
+    if(key==0){
+        deleteFirst();
+        return current;
+    }
+    if(head == NULL) {
+      return NULL;
+    }
+    for(int i=0; i<key; i++){
+        previous=current;
+        current=current->next;
+    }
+    previous->next=current->next;
+    return current;
+};
+
 void showList(struct node* head){
     printf("\n+++++++++");
     while(head!=NULL){
-        printf("%d\n",head->val);
+        printf("%d||",head->val);
         head=head->next;
     }
+    printf("\n");
+}
+int length() {
+   int length = 0;
+   struct node *current;
+
+   for(current = head; current != NULL; current = current->next) {
+      length++;
+   }
+
+   return length;
+}
+void sort() {
+
+   int i, j, k, tempData;
+   struct node *current;
+   struct node *next;
+
+   int size = length();
+   k = size ;
+    // get the largest and the lowest in each round.
+   for ( i = 0 ; i < size - 1 ; i++, k-- ) {
+      current = head;
+      next = head->next;
+
+      for ( j = 1 ; j < k ; j++ ) {
+
+         if ( current->val > next->val ) {
+            tempData = current->val;
+            current->val = next->val;
+            next->val = tempData;
+         }
+
+         current = current->next;
+         next = next->next;
+      }
+   }
 }
 
 
@@ -46,5 +125,15 @@ int main()
 {
     printf("Hello World");
     showList(Create());
+    int a;
+    scanf("%d", &a);
+    struct node* nn = delete(a);
+    printf("deleted %d",nn->val);
+    showList(head);
+
+    scanf("%d", &a);
+    struct node* aa = find(a);
+    printf("find %d",aa->val);
+    showList(head);
     return 0;
 }
